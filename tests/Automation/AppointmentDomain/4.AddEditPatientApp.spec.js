5aef54dabb6768842e754ddd5cfd521a7883cfc5
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-07-16 22:59:26 +0530

    Add edit App

--- tests/Automation/AppointmentDomain/4.AddEditPatientApp.spec.js
+++ tests/Automation/AppointmentDomain/4.AddEditPatientApp.spec.js
@@ -397,9 +397,10 @@ test.describe("Database Comparison Add Edit Patient", () => {
       await addeditpatientappointment.selectResonforReviewAppointment(jsonData.addEditAppointments[index].rea_review_reason);
       //await page.pause()
       await addeditpatientappointment.clickOnSaveButton();
+       await page.locator("xpath=//div[contains(text(),'Ok')]").click()
       await expect(page.getByText("Appointment updated successfully")).toHaveText("Appointment updated successfully");
       await page.waitForTimeout(1000);
-      await page.locator("xpath=//div[contains(text(),'Ok')]").click()
+     
       //Cancel Appointment
       await scheduleserviceapp.clickOnAppScheduleStatus();
       await scheduleserviceapp.clickOnCancelButton();
