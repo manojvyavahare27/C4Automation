15e3d9df7918363a4526afbe2d3c0e8f479599af
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-07-21 22:44:04 +0530

    Push locator changes

--- tests/Automation/PatientDomain/3.PatientDemographics.spec.js
+++ tests/Automation/PatientDomain/3.PatientDemographics.spec.js
@@ -93,7 +93,7 @@ test.describe("Patient Domain Db Comparison", () => {
         await loginpage.enter_Password(jsonData.loginDetails[0].password);
         await page.waitForTimeout(2000);
         await loginpage.clickOnLogin();
-        // await page.pause()
+         //await page.pause()
         //logger.info("Clicked on Login button successfully");
         await homepage.clickonSidebarHomeIcon()
         await homepage.clickOnPatientIcon()
